b4fcea9ecb022e76b95b94cf94af38221ccec471
MS3007 <604038653@qq.com>
2024-07-08 18:38:59 +0800

    第八天作业

---
+++ day08-hw/pages/index/index.js
@@ -0,0 +1,165 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataArray:[
+      {
+        date:"2024-05-01",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●早退"
+      },
+      {
+        date:"2024-05-02",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:56",
+        eTime:"11:05",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+      {
+        date:"2024-05-03",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●早退"
+      },
+      {
+        date:"2024-05-04",
+        beginTime:"早班8:20",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●迟到",
+        eStatus:"●早退"
+      },
+      {
+        date:"2024-05-05",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●早退"
+      },
+      {
+        date:"2024-05-06",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●早退"
+      },
+      {
+        date:"2024-05-07",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+      {
+        date:"2024-05-08",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+      {
+        date:"2024-05-09",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+      {
+        date:"2024-05-10",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+      {
+        date:"2024-05-11",
+        beginTime:"早班8:00",
+        endTime:"早班11:00",
+        sTime:"7:58",
+        eTime:"10:58",
+        sStatus:"●正常",
+        eStatus:"●正常"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
